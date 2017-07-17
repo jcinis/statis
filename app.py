15fc83b0d7f5dea9d0063ef5033115ccf26c50e7
Jessey White-Cinis <jcinis@gmail.com>
2017-07-17 01:03:50 -0400

    Test page working

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import os
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route("/")
 def hello():
-    return "Statis Server\n"
+    count = redis.incr('hits')
+    return 'Statis Server Homepage has been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
